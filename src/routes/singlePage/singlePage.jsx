60af61479f8e491d41546ded50a13b7351072515
000CL000 <claireliu9712@gmail.com>
2025-03-22 23:11:00 -0700

    Completed details content

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,5 +1,6 @@
 import './singlePage.scss';
 import Slider from '../../components/slider/Slider';
+import Map from '../../components/map/Map';
 import { singlePostData, userData } from '../../lib/dummydata';
 
 function SinglePage() {
@@ -24,15 +25,95 @@ function SinglePage() {
 
                             </div>
                         </div>
-
                         <div className="bottom">
                             {singlePostData.description}
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="features"></div>
-            <div className="wrapper"></div>
+            <div className="features">
+                <div className="wrapper">
+                    <p className="title">General</p>
+                    <div className="listVertical">
+                        <div className="feature">
+                            <img src="./utility.png" alt="" />
+                            <div className="featureText">
+                                <span>Utilities</span>
+                                <p>Renter is responsible</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="./pet.png" alt="" />
+                            <div className="featureText">
+                                <span>Pet Policy</span>
+                                <p>Pet Allowed </p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="./fee.png" alt="" />
+                            <div className="featureText">
+                                <span>Property Fees</span>
+                                <p>Must have 3x the rent in total household income</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="title">Room Sizes</p>
+                    <div className="sizes">
+                        <div className="size">
+                            <img src="./size.png" alt="" />
+                            <span>80sqft</span>
+                        </div>
+                        <div className="size">
+                            <img src="./bed.png" alt="" />
+                            <span>80sqft</span>
+                        </div>
+                        <div className="size">
+                            <img src="./bath.png" alt="" />
+                            <span>80sqft</span>
+                        </div>
+                    </div>
+                    <p className="title">Nearby Places</p>
+                    <div className="listHorizontal">
+                        <div className="feature">
+                            <img src="./school.png" alt="" />
+                            <div className="featureText">
+                                <span>School</span>
+                                <p>250m away</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="./bus.png" alt="" />
+                            <div className="featureText">
+                                <span>Bus Stop</span>
+                                <p>100m away</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="./fee.png" alt="" />
+                            <div className="featureText">
+                                <span>Restuarant</span>
+                                <p>200m away</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <p className="title">Location</p>
+                    <div className="mapContainer">
+                        < Map items={[singlePostData]} />
+                    </div>
+                    <div className="buttons">
+                        <button>
+                            <img src="./chat.png" alt="" />
+                            Send a Messege
+                        </button>
+                        <button>
+                            <img src="./save.png" alt="" />
+                            Save the Place
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div >
     )
 }
