2b526a49681304e577a0197baa1a5c749b031e66
000CL000 <claireliu9712@gmail.com>
2025-03-22 21:45:37 -0700

    Slider feature fully implemented

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -6,19 +6,37 @@ function Slider({ images = [] }) {
     if (!images.length) return null;
 
     const [imageIndex, setImageIndex] = useState(null);
+    const changeSlide = (direction) => {
+        if (direction === 'left') {
+            if (imageIndex === 0) {
+                setImageIndex(images.length - 1);
+            }
+            else {
+                setImageIndex(imageIndex - 1);
+
+            }
+        } else {
+            if (imageIndex === images.length - 1) {
+                setImageIndex(0);
+            }
+            else {
+                setImageIndex(imageIndex + 1);
+            }
+        }
+    }
 
     return (
         <div className="slider">
             {imageIndex !== null && (
                 <div className="fullSlider">
-                    <div className='arrow'>
+                    <div className='arrow' onClick={() => changeSlide('left')}>
                         <img src="/arrow.png" alt="" />
                     </div>
                     <div className='imgContainer'>
                         <img src={images[imageIndex]} alt="" />
                     </div>
                     <div className='arrow'>
-                        <img src="/arrow.png" className='right' alt="" />
+                        <img src="/arrow.png" className='right' alt="" onClick={() => changeSlide('right')} />
                     </div>
                     <div className="close" onClick={() => setImageIndex(null)}>X</div>
                 </div>
